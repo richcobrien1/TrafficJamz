dc21f4f5922c534f91139b804671b154bb138b0b
Richard O'Brien <richcobrien@hotmail.com>
2025-03-21 16:52:00 -0600

    Update to field names for group service

--- backend/src/models/group.model.js
+++ backend/src/models/group.model.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const GroupSchema = new Schema({
-  name: {
+  group_name: {
     type: String,
     required: true,
     trim: true
   },
-  description: {
+  group_description: {
     type: String,
     trim: true
   },
