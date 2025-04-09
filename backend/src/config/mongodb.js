535825cc1697f2af7d14272eddcb46c06c8afba8
Richard O'Brien <richcobrien@hotmail.com>
2025-04-09 15:25:56 -0600

    updated mongodb config

--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -6,8 +6,7 @@ require('dotenv').config();
 mongoose.set('bufferTimeoutMS', 30000);
 
 // Use the exact same connection string that works in MongoDB Compass
-const MONGODB_URI = 'mongodb+srv://richcobrien:<1Topgun123$
->@subscribers.xjpvrda.mongodb.net/?retryWrites=true&w=majority&appName=subscribers';
+const MONGODB_URI = 'mongodb+srv://richcobrien:<1Topgun123$>@subscribers.xjpvrda.mongodb.net/?retryWrites=true&w=majority&appName=subscribers';
 
 const connectMongoDB = async () => {
   try {
