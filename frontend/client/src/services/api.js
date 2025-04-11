3a49c84939a27cca9fe5957f313129d0b68308ba
Richard O'Brien <richcobrien@hotmail.com>
2025-04-11 08:01:24 -0600

    Small update to localhost and influxdb test

--- frontend/client/src/services/api.js
+++ frontend/client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with default config
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_LOCAL_URL || '/api',
+  baseURL: process.env.REACT_APP_API_LOCAL_URL || 'http://localhost:3001/api',
   headers: {
     'Content-Type': 'application/json'
   }
