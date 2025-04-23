597dbd3af62de23b115f9d4300a45024fbd8044e
Richard O'Brien <richcobrien@hotmail.com>
2025-04-23 17:12:33 -0600

    Revert api base url chnage

--- frontend/client/src/services/api.js
+++ frontend/client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: process.env.BACKEND_API_URL || 'http://localhost:3000',
+  baseURL: process.env.BACKEND_API_URL || 'http://localhost:3001',
   withCredentials: false,
   headers: {
     'Content-Type': 'application/json'
