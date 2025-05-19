cf6d6db8c9f11d0227198dca278794231cf0e156
Richard O'Brien <richcobrien@hotmail.com>
2025-05-19 08:05:10 -0600

    hello

--- backend/hello.js
+++ backend/hello.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
     res.status(200).json({
-      message: 'Hello from Traffic Jam API!',
+      message: '=========================================   Hello from TrafficJam API!   ===============================',
       timestamp: new Date().toISOString(),
       environment: process.env.NODE_ENV || 'development'
     });
