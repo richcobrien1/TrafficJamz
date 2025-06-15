a0f8d2791612e7ac56f450028c4bee0bf41375f2
Richard O'Brien <richcobrien@hotmail.com>
2025-06-14 18:07:12 -0600

    Update Dockerfile builds with plain logs

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,4 @@
 # docker build -t trafficjamz-backend ./backend
 # docker build -t trafficjamz-backend -f Dockerfile .
-docker build -t trafficjamz-fullstack -f Dockerfile .
+# docker build -t trafficjamz-fullstack -f Dockerfile .
+docker build --progress=plain -t trafficjamz-fullstack -f Dockerfile .
--- docker_build_fe_image.sh
+++ docker_build_fe_image.sh
@@ -1,2 +1,2 @@
 # docker build -t trafficjamz-frontend ./frontend
-docker build -t trafficjamz-frontend -f frontend/Dockerfile .
+docker build --progress=plain -t trafficjamz-frontend -f frontend/Dockerfile .
