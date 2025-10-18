52d241836c936c5ecdd1182624a67e00bf185909
richcobrien <richcobrien@users.noreply.github.com>
2025-10-18 09:23:26 -0600

    Update prod CORS

--- jamz-server/src/index.js
+++ jamz-server/src/index.js
@@ -68,7 +68,7 @@ const allowedOrigins = [
   process.env.CORS_ORIGIN_DEV || 'http://localhost:5175',      // TrafficJamz frontend dev (http)
   process.env.CORS_ORIGIN_DEV_HTTPS || 'https://localhost:5175',// TrafficJamz frontend dev (https)
   process.env.CORS_ORIGIN_PROD || 'https://jamz.v2u.us',     // TrafficJamz frontend prod (http)
-  process.env.CORS_ORIGIN_PROD_HTTPS || 'https://localhost:8080', // TrafficJamz frontend prod (https)
+  process.env.CORS_ORIGIN_PROD_HTTPS || 'https://jamz.v2u.us', // TrafficJamz frontend prod (https)
   'https://trafficjam.v2u.us',       // Production client
   'capacitor://trafficjam.v2u.us',   // iOS apps
   'ionic://trafficjam.v2u.us',       // Android apps
