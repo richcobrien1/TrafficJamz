f296cfc66f9c63f34645644ad864ffdb5554e088
richcobrien <richcobrien@users.noreply.github.com>
2025-10-18 14:05:34 -0600

    Backend update

--- jamz-server/api/index.js
+++ jamz-server/api/index.js
@@ -1,3 +1,9 @@
+import app from '../server/app'; // adjust path to your Express app
+
+export default function handler(req, res) {
+  app(req, res);
+}
+
 // api/index.js
 const app = require('../src/index'); // import your Express app
 module.exports = app;
