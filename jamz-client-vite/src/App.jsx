4c5ca26a99eb02adddd44dd49c427966c7d62782
richcobrien <richcobrien@users.noreply.github.com>
2025-10-26 20:51:07 -0600

    Add /login route alias for easier login access

--- jamz-client-vite/src/App.jsx
+++ jamz-client-vite/src/App.jsx
@@ -90,6 +90,7 @@ function App() {
             >
               <Routes>
                 {/* Public routes */}
+                <Route path="/login" element={<Login />} />
                 <Route path="/auth/login" element={<Login />} />
                 <Route path="/auth/register" element={<Register />} />
                 <Route path="/auth/forgot-password" element={<ForgotPassword />} />
